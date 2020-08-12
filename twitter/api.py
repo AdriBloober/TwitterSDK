e9c86cedbe48303b9a08171a88b8991f9af376b7
adribloober <adrianbloober@gmail.com>
2020-08-12 22:07:24 +0200

    Add tweets and users

--- twitter/api.py
+++ twitter/api.py
@@ -1,7 +1,8 @@
+from twitter.endpoints.tweets.api import TweetsApi
 from twitter.endpoints.users.api import UserApi, FriendsFollowersApi
 
 
-class TwitterApi(UserApi, FriendsFollowersApi):
+class TwitterApi(UserApi, FriendsFollowersApi, TweetsApi):
     """TwitterAPI with subapi's."""
 
     pass
--- twitter/api_client.py
+++ twitter/api_client.py
@@ -3,7 +3,7 @@ from requests_oauthlib import OAuth1
 
 from twitter.error_mangement import parse_error
 from twitter.paths import PathOperation
-
+from typing import Union
 
 class ApiClient:
     request_session = None
@@ -22,7 +22,9 @@ class ApiClient:
     def api_path(self):
         return self.basic_path + "/" + self.api_version
 
-    def parse_path(self, path: PathOperation):
+    def parse_path(self, path: Union[PathOperation, str]):
+        if type(path) == str:
+            return path
         p = self.basic_path
         if path.version_requirement:
             p += "/" + self.api_version
@@ -53,4 +55,10 @@ class TwitterRawApi:
         self.client = ApiClient(basic_path=basic_path, api_version=api_version)
 
     def full_authenticated_request(self, method, path, **kwargs):
+        if "params" in kwargs:
+            new_params = {}
+            for k, v in kwargs["params"].items():
+                if not (v is None or v == "" or v == []):
+                    new_params[k] = v
+            kwargs["params"] = new_params
         return self.client.request(method, path, auth=self.auth, **kwargs)
